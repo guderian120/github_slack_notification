e62af80b34f89a54197967ce6b4a6976d5c2ba9a
guderian120 <realamponsah10@yahoo.com>
2025-05-28 15:57:34 +0000

    added depth to readme

--- cool_function.py
+++ cool_function.py
@@ -1,6 +1,6 @@
 # cool_function.py
 class CoolClass:
-    def cool_attribute(self):  # Now it's a method
+    def cool_attribute(self):  # Cool attribute method
         return "This is a cool attribute!"
 if __name__ == "__main__":
     cool_instance = CoolClass()
