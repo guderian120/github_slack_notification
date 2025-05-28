5a0011e9f8f3e36361593565d419d64badfced16
guderian120 <realamponsah10@yahoo.com>
2025-05-28 15:33:30 +0000

    first commit

---
+++ cool_function.py
@@ -0,0 +1,7 @@
+# cool_function.py
+class CoolClass:
+    def cool_attribute(self):  # Now it's a method
+        return "This is a cool attribute!"
+if __name__ == "__main__":
+    cool_instance = CoolClass()
+    print(cool_instance.cool_method())
